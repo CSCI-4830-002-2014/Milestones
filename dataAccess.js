d0a2647028a3ac28aece17a4ccbbd3b058204fd4
Peyman Mortazavi <pmortazavi@cailmobility.com>
2014-12-02 00:36:20 -0700

    added dataaccess, GET words

--- dataAccess.js
+++ dataAccess.js
@@ -1,20 +1,32 @@
 
 var mongo = require('mongojs');
 var fs = require('fs');
+var linq = require('node-linq').LINQ;
 
 var configObject = JSON.parse(fs.readFileSync('configs.json'));
 var db = mongo(configObject.database.connectionString, ["audios"]);
 
-// words: array of string
+
+// words: array of { word: <string>, count: <string> }
 exports.addWords = function addWords (words) {
+
+	for(var i = 0; i < words.length; i++) {
+
+		db.audios.update( { word: words[i].word }, {$inc: {count: words[i].count}}, {upsert:true} );
+
+	}
 
 }
 
 // word: string
-exports.getCountForWord = function getCountForWord (word) {
+exports.getWordInfo = function getCountForWord (word, callback) {
+
+	return db.audios.findOne( { word: word }, callback);
 
 }
 
-exports.getAllWords = function getAllWords () {
+exports.getAllWords = function getAllWords (callback) {
+
+	db.audios.find().toArray(callback);
 
 }
--- main.js
+++ main.js
@@ -22,14 +22,53 @@ function startServer()
 
 startServer()
 
+function clone(object) {
+	return JSON.parse(JSON.stringify(object));
+}
+
 app.get("/audio", function(request, response) {
 
-	response.send( [ { word: "hi", count: 1}, { word: "good bye", count: 5 } ] );
+	dataAccess.getAllWords( function (error, docs) {
+
+			
+		if(error != null)
+		{
+			response.send( "bad request" );
+			return;
+		}
+
+		var result = new linq(docs).Select(function(x) {
+			var model = clone(x);
+			delete model._id;
+			return model;
+		} ).ToArray();
+
+		response.send(result);
+
+	});
+
+});
+
+app.get("/audio/:word", function(request, response) {
+
+	dataAccess.getWordInfo(request.param("word"), function(error, doc) {
+
+		if(doc == null) {
+			response.status(404).send({"msg":"Not Found"});
+			return;
+		}
+		
+		var model = clone(doc);
+		delete model._id;
+		response.send( model );
+
+	});
 
 });
 
 app.post("/audio", function(request, response) {
 
-	response.send( request.body );
+	dataAccess.addWords( [ {word:"peyman", count: 1}, {word:"kate",count:2} ] );
+	response.send(request.body);
 
 });
